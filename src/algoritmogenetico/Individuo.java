40ae8a02f120872f8e758d3bb725396bdb976be1
Bolsista <Bolsista>
2019-08-28 11:07:44 -0300

    Algoritmo concluido

--- src/algoritmogenetico/Individuo.java
+++ src/algoritmogenetico/Individuo.java
@@ -14,7 +14,7 @@ public class Individuo implements Comparable<Individuo> {
     //atributos do problema especifico
     private double qtdMilho;
     private double qtdSoja;
-    
+
     private ArrayList<Item> itens;
 
     //cria um individuo aleatorio da primeira geracao
@@ -25,13 +25,16 @@ public class Individuo implements Comparable<Individuo> {
             this.setQtdItens();
         } while (!validar());
         avaliar();
+        System.out.println(">>>> Validado <<<<<");
+        System.out.println("peso = " + getPeso());
+        System.out.println("Valor = "+ aptidao);
     }
 
     // cria um individuo a partir de genes definidos
     public Individuo(int[] genes) {
         itens = new ArrayList<>();
         itens.addAll(Genetico.mochila);
-        for(int i = 0; i < itens.size(); i++){
+        for (int i = 0; i < itens.size(); i++) {
             itens.get(i).setQtd(genes[i]);
         }
 
@@ -40,24 +43,21 @@ public class Individuo implements Comparable<Individuo> {
             int posAleatoria = random.nextInt(genes.length); //define gene que sera mutado
             mutacao(posAleatoria);
         }
-        avaliar2();
+        avaliar();
     }
 
     private boolean validar() {
         int peso = 0;
         for (Item item : itens) {
-            if(item.getQtd() != 0){
+            if (item.getQtd() != 0) {
                 peso = peso + item.getPeso() * item.getQtd();
             }
         }
-        
-        if(peso < Genetico.PESO_MAXIMO){
-        setPeso(peso);
-        //itens.clear();
-        //itens.addAll(Genetico.mochila);
-        //System.out.println("Atribuiu peso = "+ getPeso());
+
+        if (peso < Genetico.PESO_MAXIMO) {
+            setPeso(peso);
             return true;
-        }else{
+        } else {
             return false;
         }
         //return peso < Genetico.PESO_MAXIMO;
@@ -66,29 +66,25 @@ public class Individuo implements Comparable<Individuo> {
     private void mutacao(int posicao) {
         do {
             Random r = new Random();
-            
-            itens.get(posicao).setQtd(r.nextInt(5+1));
-            
-            
+
+            itens.get(posicao).setQtd(r.nextInt(5 + 1));
+
         } while (!validar());
-            avaliar();
+        avaliar();
     }
 
-    
-    private void setQtdItens(){
+    private void setQtdItens() {
         itens.forEach((item) -> {
-            item.setQtd(random.nextInt(5+1));
+            item.setQtd(random.nextInt(5 + 1));
         });
-        /*
+
         System.out.println("---------------------------------------------");
-        Genetico.mochila.forEach((item) -> {    
-            System.out.println("Item "+ item.getNome()+ " Qtd = "+ item.getQtd()
-            + " || valor/unidade = "+ item.getValor() + " peso = "+ item.getPeso());
-        });*/
+        Genetico.mochila.forEach((item) -> {
+            System.out.println("Item " + item.getNome() + " Qtd = " + item.getQtd()
+                    + " || valor/unidade = " + item.getValor() + " peso = " + item.getPeso());
+        });
     }
-    
-    
-    
+
     private void setQtdMilho() {
         this.qtdMilho = random.nextInt(10);
     }
@@ -101,68 +97,48 @@ public class Individuo implements Comparable<Individuo> {
         return aptidao;
     }
 
-    public int[] getGenes() { 
+    public int[] getGenes() {
         int genes[] = new int[itens.size()];
-        for(int i = 0; i < itens.size(); i++){
+        for (int i = 0; i < itens.size(); i++) {
             genes[i] = itens.get(i).getQtd();
         }
-        
+
         return genes;
     }
 
     private void avaliar() {
         Double valor = 0.0;
         for (Item item : itens) {
-            if(item.getQtd() != 0){
-            valor = valor + (item.getValor() * item.getQtd());
+            if (item.getQtd() != 0) {
+                valor = valor + (item.getValor() * item.getQtd());
             }
-             
-        }     
+
+        }
         aptidao = valor;
-        //System.out.println("Valor = "+ aptidao);
     }
-    
-    private void avaliar2() {
-        Double valor = 0.0;
-        for (Item item : itens) {
-            if(item.getQtd() != 0){
-            valor = valor + (item.getValor() * item.getQtd());
-            }
-             
-        }   
-        aptidao = valor;
-        //aptidao = valor; System.out.println("Aptidao print = " + aptidao);
-        //System.out.println("Valor = "+ aptidao);
-    }
+
+   
 
     @Override
     public int compareTo(Individuo i) {
         return this.aptidao.compareTo(i.aptidao);
-        /*
-        if (this.aptidao <= i.aptidao) {
-            return -1;
-        }
-        if (this.aptidao >= i.aptidao) {
-            return 1;
-        }
-        return 0;*/
     }
 
     public int getPeso() {
         int pesoTotal = 0;
         for (Item item : itens) {
-            if(item.getQtd() != 0){
+            if (item.getQtd() != 0) {
                 pesoTotal = pesoTotal + item.getPeso() * item.getQtd();
             }
         }
-        
+
         return pesoTotal;
     }
 
     public void setPeso(int peso) {
         this.pesoTotal = peso;
     }
-    
+
     @Override
     public String toString() {
         return "Cromossomo " + Arrays.toString(getGenes()) + " Aptidao: " + aptidao + "\n"
--- src/algoritmogenetico/Principal.java
+++ src/algoritmogenetico/Principal.java
@@ -5,8 +5,6 @@ public class Principal {
     public static void main(String[] args) {
         Genetico AG = new Genetico();
         AG.Iniciar();
-            
-        
         
     }
 }
